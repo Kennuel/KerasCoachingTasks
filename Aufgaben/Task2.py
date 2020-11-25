dd4ec933bb3256208bed2cfb9b8b6cefa04c6136
Hendrik Wiechula <45036460+Kennuel@users.noreply.github.com>
2020-11-25 17:13:55 +0100

    Update and rename Aufgabe2.py to Task2.py

--- Aufgaben/Task2.py
+++ Aufgaben/Task2.py
@@ -1,49 +1,49 @@
 import tensorflow as tf
  
-#Datenladen
+# Load data
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# Datenvorbereiten
+# Normalize your data
 x_train = tf.keras.utils.normalize(x_train, 1)
 x_test = tf.keras.utils.normalize(x_test, 1)
 
+# Reshabe to fit into Convolutional Layers
 CHANNELS = 1
 x_train = x_train.reshape(x_train.shape[0], 28, 28, CHANNELS)
 x_test = x_test.reshape(x_test.shape[0], 28, 28, CHANNELS)
  
-# Modelaufbauen
+# Build Model
 
 model = tf.keras.models.Sequential()
 
 
 model.add(tf.keras.layers.Conv2D(28, (4, 4), activation='relu', input_shape=(28, 28, CHANNELS)))
 
-# TODO: Füge ein max pooling layer hinzu
+# TODO: Add a max-pooling layer
 
-# TODO: Füge eine oder mehrere Conv2D layer hinzu mit bspw. activation=tf.nn.relu
+# TODO: Add additional Conv2D layers activation=tf.nn.relu
 
-# TODO: Füge eine Dropoutlayer layer hinzu mit bspw. activation=tf.nn.relu
+# TODO: Add a dropout layer. activation=tf.nn.relu
 
-# TODO: Was fehlt hier, damit es auf das dense ausgabe layer passt?
+# TODO: Whats needed to get a Conv2D Output to a fit to the output layer?
 
-# TODO: Was kann noch verbessert werden?
+# TODO: What could be added here additionally?
 
 model.add(tf.keras.layers.Dense(10,activation=tf.nn.softmax))
 
  
 
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy',metrics=['accuracy'] )
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'] )
 
 # Tensorboard
 # Load with tensorboard --logdir ./Graph
 tbCallBack = tf.keras.callbacks.TensorBoard(log_dir='./Graph', histogram_freq=0, write_graph=True, write_images=True)
 
-#Modeltrainieren
+#Train your model
 model.fit(x_train,y_train, epochs=3, callbacks=[tbCallBack])
 
-# Auswerten
-
+# evaluate
 val_loss, val_acc = model.evaluate(x_test, y_test)
 print("")
 print("val_loss:", val_loss)
